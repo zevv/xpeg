dee131a0720cc7f8987181c36ee66f97ffa8bb7b
Ico Doornekamp <ico@zevv.nl>
2021-12-27 23:08:25 +0100

    Cleanups

--- lib/parsepatt.ex
+++ lib/parsepatt.ex
@@ -1,7 +1,48 @@
 
 defmodule Parsepatt do
 
-  import Patt
+  # Emit a choice/commit pair around pattern p; off_back and off_commit are the
+  # offsets to the backtrack and commit targets, relative to the commit
+  # instruction
+  def choice_commit(p, off_commit, off_back) do
+    [{:choice, off_back, off_commit}] ++ p ++ [{:commit}]
+  end
+
+  # Generic ordered choice
+  def mk_choice(p1, p2) do
+    choice_commit(p1, length(p1) + length(p2) + 2, length(p1) + 2) ++ p2
+  end
+
+  # kleene-star-operator for sets make a :span
+  #def mk_star(set: cs) do
+  #  [{:span, cs}]
+  #end
+
+  # Generic kleene-star operator
+  def mk_star(p) do
+    choice_commit(p, 0, length(p) + 2)
+  end
+
+  # Generic ! 'not' predicate
+  def mk_not(p) do
+    choice_commit(p, length(p) + 2, length(p) + 3) ++ [{:fail}]
+  end
+
+  # Generic optional
+  def mk_optional(p) do
+    choice_commit(p, length(p) + 2, length(p) + 2)
+  end
+
+  # Minus for sets is the difference between sets
+  def mk_minus([set: cs1], set: cs2) do
+    [set: MapSet.difference(cs1, cs2)]
+  end
+
+  # Generic minus, !p2 * p1
+  def mk_minus(p1, p2) do
+    List.flatten([mk_not(p2), p1])
+  end
+
 
   # Transform AST tuples into PEG IR
   def parse({id, lineinfo, args}) do
@@ -72,24 +113,12 @@ defmodule Parsepatt do
       [{:set, Range.new(lo, hi) |> Enum.into([]) |> List.flatten() |> MapSet.new() }]
 
       # Code block
-      {:&, [code]} ->
-        [{:code, substitute_ampersands(code)}]
-
-      # Code block
       {:fn, [code]} ->
         [{:code, {:fn, lineinfo, [code]}}]
 
       e -> raise("XPeg: Syntax error at '#{Macro.to_string(e)}' \n\n   #{inspect(e)}\n\n")
-
     end
-
   end
-
-
-  def parse(atom) when is_atom(atom) do
-    [{:call, atom}]
-  end
-
 
   # Handler for funny AST in `{}` charsets
   def parse({p1, p2}) do
@@ -104,7 +133,6 @@ defmodule Parsepatt do
     end
   end
 
-
   # Transform AST literals into PEG IR
   def parse(p) do
     case p do
@@ -114,23 +142,6 @@ defmodule Parsepatt do
       v when is_binary(v) -> to_charlist(v) |> Enum.map(fn c -> {:chr, c} end)
       [v] -> [{:chr, v}]
       v -> raise("Unhandled lit: #{inspect(v)}")
-    end
-  end
-
-  defp substitute_ampersands(n) do
-    Macro.postwalk(n, fn
-      {:&, li, [idx]} -> {{:., li, [{:__aliases__, li, [:Enum]}, :at]}, li, [{:captures, li, nil}, idx-1]}
-      other -> other
-    end)
-  end
-
-  defp substitute_ampersands2(n) do
-    case n do
-      {:&, li, [idx]} -> {{:., li, [{:__aliases__, li, [:Enum]}, :at]}, li, [{:captures, li, nil}, idx-1]}
-      {name, li, kids} when is_list(kids) -> {name, li, Enum.map(kids, &substitute_ampersands/1)}
-      l when is_list(l) -> Enum.map(l, &substitute_ampersands/1)
-      {a, b} -> {substitute_ampersands(a), substitute_ampersands(b)}
-      n -> n
     end
   end
 
@@ -146,7 +157,4 @@ defmodule Parsepatt do
     end)
     s
   end
-
-
 end
-
