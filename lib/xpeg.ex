99c4f4742793304b2ebc83190985403fae00e63b
Ico Doornekamp <ico@zevv.nl>
2021-12-29 14:59:48 +0100

    syntax fix

--- lib/xpeg.ex
+++ lib/xpeg.ex
@@ -274,7 +274,7 @@ defmodule Xpeg do
 
     p = peg :list do
       :list <- :word * star( "," * :word )
-      :word <- cap(+{'0'..'9'}) * fn [v|cs] -> String.to_integer(v)|cs]
+      :word <- cap(+{'0'..'9'}) * fn [v|cs] -> String.to_integer(v)|cs] end
     end
 
     match(p, "123,42,31415")
