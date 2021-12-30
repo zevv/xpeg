7f9db63ca11321a521e6a26a161589555c8ba03c
Ico Doornekamp <ico@zevv.nl>
2021-12-30 22:48:00 +0100

    records

--- lib/xpeg.ex
+++ lib/xpeg.ex
@@ -1,4 +1,16 @@
 defmodule Xpeg do
+
+  require Record
+  
+  Record.defrecord(:state, 
+    func: :nil,
+    back_stack: [],
+    ret_stack: [],
+    captures: [],
+    cap_stack: [],
+    status: :running,
+  )
+
   @moduledoc """
 
     XPeg is a pure Elixir pattern matching library. It provides macros to compile
@@ -37,13 +49,11 @@ defmodule Xpeg do
     end
   end
 
-  def collect_captures(state) do
+  def collect_captures(cap_stack) do
     {cap_stack, captures} =
-      state.cap_stack
+      cap_stack
       |> Enum.reverse()
       |> Xpeg.collect_captures([], [])
-
-    %{state | cap_stack: cap_stack, captures: captures ++ state.captures}
   end
 
   def trace(ip, cmd, s) do
@@ -56,45 +66,49 @@ defmodule Xpeg do
     case inst do
       {:nop} ->
         quote do
-          {state, s, si, unquote(ip + 1)}
+        {ctx, s, si, unquote(ip + 1)}
         end
 
       {:any, n} ->
         quote do
           if Enum.count(s) >= unquote(n) do
-            {state, Enum.drop(s, unquote(n)), si + unquote(n), unquote(ip + 1)}
+            {ctx, Enum.drop(s, unquote(n)), si + unquote(n), unquote(ip + 1)}
           else
-            {state, s, si, :fail}
+            {ctx, s, si, :fail}
           end
         end
 
       {:chr, c} ->
         quote do
           case s do
-            [unquote(c) | s] -> {state, s, si + 1, unquote(ip + 1)}
-            _ -> {state, s, si, :fail}
+            [unquote(c) | s] -> {ctx, s, si + 1, unquote(ip + 1)}
+            _ -> {ctx, s, si, :fail}
           end
         end
 
       {:set, cs} ->
         quote do
           case s do
-            [c | s] when c in unquote(MapSet.to_list(cs)) -> {state, s, si + 1, unquote(ip + 1)}
-            _ -> {state, s, si, :fail}
+            [c | s] when c in unquote(MapSet.to_list(cs)) -> {ctx, s, si + 1, unquote(ip + 1)}
+            _ -> {ctx, s, si, :fail}
           end
         end
 
       {:span, cs} ->
         quote do
           {s1, s2} = Enum.split_while(s, fn c -> c in unquote(MapSet.to_list(cs)) end)
-          {state, s2, si + Enum.count(s1), unquote(ip) + 1}
+          {ctx, s2, si + Enum.count(s1), unquote(ip) + 1}
         end
 
       {:return} ->
         quote do
-          case state.ret_stack do
-            [ip | rest] -> {%{state | ret_stack: rest}, s, si, ip}
-            [] -> {%{state | status: :ok}, s, si, ip}
+          case Xpeg.state(ctx, :ret_stack) do
+            [ip | ret_stack] -> 
+              ctx = Xpeg.state(ctx, ret_stack: ret_stack)
+              {ctx, s, si, ip}
+            [] ->
+              ctx = Xpeg.state(ctx, status: :ok)
+              {ctx, s, si, ip}
           end
         end
 
@@ -103,71 +117,77 @@ defmodule Xpeg do
           frame = %{
             ip_back: ip + unquote(off_back),
             ip_commit: ip + unquote(off_commit),
-            ret_stack: state.ret_stack,
-            cap_stack: state.cap_stack,
+            ret_stack: Xpeg.state(ctx, :ret_stack),
+            cap_stack: Xpeg.state(ctx, :cap_stack),
             s: s,
             si: si
           }
 
-          state = %{state | back_stack: [frame | state.back_stack]}
-          {state, s, si, unquote(ip + 1)}
+          back_stack = Xpeg.state(ctx, :back_stack)
+          ctx = Xpeg.state(ctx, back_stack: [frame | back_stack])
+          {ctx, s, si, unquote(ip + 1)}
         end
 
       {:commit} ->
         quote do
-          [frame | back_stack] = state.back_stack
-          state = %{state | back_stack: back_stack}
-          {state, s, si, frame.ip_commit}
+          [frame | back_stack] = Xpeg.state(ctx, :back_stack)
+          ctx = Xpeg.state(ctx, back_stack: back_stack)
+          {ctx, s, si, frame.ip_commit}
         end
 
       {:call, addr} ->
         quote do
-          state = %{state | ret_stack: [ip + 1 | state.ret_stack]}
-          {state, s, si, unquote(addr)}
+          ret_stack = Xpeg.state(ctx, :ret_stack)
+          ctx = Xpeg.state(ctx, ret_stack: [ip+1 | ret_stack])
+          {ctx, s, si, unquote(addr)}
         end
 
       {:jump, addr} ->
         quote do
-          {state, s, si, unquote(addr)}
+          {ctx, s, si, unquote(addr)}
         end
 
       {:capopen} ->
         quote do
-          state = %{state | cap_stack: [{:open, s, si} | state.cap_stack]}
-          {state, s, si, unquote(ip + 1)}
+          cap_stack = Xpeg.state(ctx, :cap_stack)
+          ctx = Xpeg.state(ctx, cap_stack: [{:open, s, si} | cap_stack])
+          {ctx, s, si, unquote(ip + 1)}
         end
 
       {:capclose} ->
         quote do
-          state = %{state | cap_stack: [{:close, s, si} | state.cap_stack]}
-          {state, s, si, unquote(ip + 1)}
+          cap_stack = Xpeg.state(ctx, :cap_stack)
+          ctx = Xpeg.state(ctx, cap_stack: [{:close, s, si} | cap_stack])
+          {ctx, s, si, unquote(ip + 1)}
         end
 
       {:code, code} ->
         quote do
-          state = Xpeg.collect_captures(state)
+          cap_stack = Xpeg.state(ctx, :cap_stack)
+          {cap_stack, captures} = Xpeg.collect_captures(cap_stack)
+          ctx = Xpeg.state(ctx, cap_stack: cap_stack, captures: captures ++ Xpeg.state(ctx, :captures))
           func = unquote(code)
-          captures = func.(state.captures)
-          state = %{state | captures: captures}
-          {state, s, si, unquote(ip + 1)}
+          captures = func.(Xpeg.state(ctx, :captures))
+          ctx = Xpeg.state(ctx, captures: captures)
+          {ctx, s, si, unquote(ip + 1)}
         end
 
       {:fail} ->
         quote do
-          case state.back_stack do
+          back_stack = Xpeg.state(ctx, :back_stack)
+          case back_stack do
             [frame | back_stack] ->
-              state = %{
-                state
-                | back_stack: back_stack,
-                  ret_stack: frame.ret_stack,
-                  cap_stack: frame.cap_stack
-              }
+              ctx = Xpeg.state(ctx,
+                back_stack: back_stack,
+                ret_stack: frame.ret_stack,
+                cap_stack: frame.cap_stack,
+              )
 
-              {state, frame.s, frame.si, frame.ip_back}
+              {ctx, frame.s, frame.si, frame.ip_back}
 
             [] ->
-              state = %{state | status: :error}
-              {state, s, si, 0}
+              ctx = Xpeg.state(ctx, status: :error)
+              {ctx, s, si, 0}
           end
         end
     end
@@ -196,18 +216,20 @@ defmodule Xpeg do
 
     f =
       quote do
-        fn state, s, si, ip ->
-          {state, s, si, ip} =
+        fn ctx, s, si, ip ->
+
+          {ctx, s, si, ip} =
             case ip do
               unquote(cases)
             end
 
-          case state.status do
+          func = state(ctx, :func)
+          
+          case Xpeg.state(ctx, :status) do
             :running ->
-              state.func.(state, s, si, ip)
-
+              func.(ctx, s, si, ip)
             _ ->
-              %{state | match_len: si}
+              {ctx, si}
           end
         end
       end
@@ -311,31 +333,19 @@ defmodule Xpeg do
   end
 
   def match(func, s) do
-    state = %{
-      func: func,
-      time: 0,
-      status: :running,
-      back_stack: [],
-      ret_stack: [],
-      cap_stack: [],
-      captures: [],
-      match_len: 0,
+    ctx = state(func: func)
+
+    {time, {ctx, match_len}} = :timer.tc(fn ->
+      func.(ctx, String.to_charlist(s), 0, 0)
+    end)
+
+    {_, captures} = collect_captures(state(ctx, :cap_stack))
+    %{
+      captures: captures ++ state(ctx, :captures),
+      status: state(ctx, :status),
+      time: time,
+      match_len: match_len
     }
-
-    s = String.to_charlist(s)
-    {time, state} = :timer.tc(fn -> func.(state, s, 0, 0) end)
-    state = %{state | time: time / 1.0e6}
-    collect_captures(state)
-  end
-
-  def run() do
-    p =
-      peg :flop, debug: false, trace: false, dump_ir: false do
-        :flop <- :flip
-        :flip <- {'a'..'b'}
-      end
-
-    match(p, "abcdefghi")
   end
 
 end
