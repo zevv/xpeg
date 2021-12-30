3187c1a487de89f8d29942b64cf0461aa5e13e45
Ico Doornekamp <ico@zevv.nl>
2021-12-30 14:50:45 +0100

    cleanups

--- lib/parsepatt.ex
+++ lib/parsepatt.ex
@@ -42,6 +42,18 @@ defmodule Parsepatt do
     List.flatten([mk_not(p2), p1])
   end
 
+  # Transform AST character set to PEG IR. `{'x','y','A'..'F','0'}`
+  defp parse_set(ps) do
+    s = Enum.reduce(ps, MapSet.new(), fn p, s ->
+      case p do
+        [v] -> MapSet.put(s, v)
+        v ->
+          [{:set, s2}] = parse(v)
+          MapSet.union(s, s2)
+      end
+    end)
+    s
+  end
 
   # Transform AST tuples into PEG IR
   def parse({id, lineinfo, args}) do
@@ -53,46 +65,39 @@ defmodule Parsepatt do
       {:__block__, ps} ->
         Enum.reduce(ps, [], fn rule, acc -> parse(rule) ++ acc end)
 
-      # Named rule
-      #{:<-, [{label, _, nil}, patt]} ->
-      #  [{label, parse(patt) ++ [{:return}]}]
-      
       {:<-, [label, patt]} ->
         [{label, parse(patt) ++ [{:return}]}]
 
-      #{:<-, [{:__aliases__, _, [label]}, patt]} ->
-      #  [{label, parse(patt) ++ [{:return}]}]
-
-      # '*' Concatenation
+      # infix: '*' Concatenation
       {:*, [p1, p2]} ->
         parse(p1) ++ parse(p2)
 
-      # '|' Ordered choice
+      # infix '|': Ordered choice
       {:|, [p1, p2]} ->
         mk_choice(parse(p1), parse(p2))
 
-      # '*' zero-or-more operator
+      # prefix '*': zero-or-more operator
       {:star, [p]} ->
         mk_star(parse(p))
 
-      # one-or-zero operator
+      # prefix '?': one-or-zero operator
       {:opt, [p]} ->
         mk_opt(parse(p))
 
-      # '+' one-or-more operator
+      # prefix '+': one-or-more operator
       {:+, [p]} ->
         p = parse(p)
         p ++ mk_star(p)
 
-      # Difference
+      # Infix '-': difference
       {:-, [p1, p2]} ->
         mk_minus(parse(p1), parse(p2))
 
-      # '!' 'not' operator
+      # prefix '!': 'not' operator
       {:!, [p]} ->
         mk_not(parse(p))
       
-      # '&' 'and-predicate' operator
+      # prefix '&': 'and-predicate' operator
       {:&, [p]} ->
         mk_not(mk_not(parse(p)))
 
@@ -111,11 +116,11 @@ defmodule Parsepatt do
 
       # Capture
       {:cap, [p]} ->
-        List.flatten([{:capopen}, parse(p), {:capclose}])
+        [{:capopen}] ++ parse(p) ++ [{:capclose}]
 
       # Char range
       {:.., [[lo], [hi]]} ->
-      [{:set, Range.new(lo, hi) |> Enum.into([]) |> List.flatten() |> MapSet.new() }]
+        [{:set, MapSet.new(lo..hi)}]
 
       # Code block
       {:fn, [code]} ->
@@ -129,7 +134,7 @@ defmodule Parsepatt do
   # Charsets
   def parse({p1, p2}) do
     case {parse(p1), parse(p2)} do
-      {[{:chr, a}], [{:chr,b}]} -> [{:set, MapSet.new([a,b])}]
+      {[{:chr, a}], [{:chr, b}]} -> [{:set, MapSet.new([a,b])}]
       {[{:set, a}], [{:set, b}]} -> [{:set, MapSet.union(a, b)}]
       {[{:set, a}], [{:chr, b}]} -> [{:set, MapSet.put(a, b)}]
       {[{:chr, a}], [{:set, b}]} -> [{:set, MapSet.put(b, a)}]
@@ -147,19 +152,6 @@ defmodule Parsepatt do
       [v] -> [{:chr, v}]
       v -> raise("Unhandled lit: #{inspect(v)}")
     end
-  end
-
-  # Transform AST character set to PEG IR. `{'x','y','A'..'F','0'}`
-  defp parse_set(ps) do
-    s = Enum.reduce(ps, MapSet.new(), fn p, s ->
-      case p do
-        [v] -> MapSet.put(s, v)
-        v ->
-          [{:set, s2}] = parse(v)
-          MapSet.union(s, s2)
-      end
-    end)
-    s
   end
 
 end
