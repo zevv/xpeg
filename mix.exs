480ca73b4cc7a56306c461bdf57280586aa500c5
Ico Doornekamp <ico@zevv.nl>
2022-01-12 06:57:32 +0100

    0.7.2: removed deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Xpeg.MixProject do
   def project do
     [
       app: :xpeg,
-      version: "0.7.1",
+      version: "0.7.2",
       description: "Native Elixir PEG (Parsing Expression Grammars) string matching library",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
@@ -28,21 +28,19 @@ defmodule Xpeg.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.14"},
-      {:exprof, "~> 0.2.0"},
-      {:poison, "~> 5.0"},
-      {:jason, "~> 1.2"},
-      {:eflame, "~> 1.0", only: [:dev]},
+      {:ex_doc, "~> 0.14", only: [:dev], runtime: false},
+      #{:exprof, "~> 0.2.0", only: [:dev, :test], runtime: false},
+      #{:poison, "~> 5.0", only: [:dev, :test], runtime: false},
+      #{:jason, "~> 1.2", only: [:dev, :test], runtime: false},
+      #{:eflame, "~> 1.0", only: [:dev, :test], runtime: false},
     ]
   end
 end
